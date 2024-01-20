9d4e61491520b66e9b815583b4bad92863e82a37
bearthatcares <bearthatcares.eve@gmail.com>
2024-01-20 06:46:00 -0500

    Add whitenoise for serving static files

--- tools/settings_common.py
+++ tools/settings_common.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # static files
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

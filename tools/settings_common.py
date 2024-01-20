b1836ffabe286c901d18d25511005c9656092338
bearthatcares <bearthatcares.eve@gmail.com>
2024-01-20 06:38:27 -0500

    Serve static files on tools.minmatar.org

--- tools/settings_common.py
+++ tools/settings_common.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,7 +138,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(
+    BASE_DIR,
+    "static",
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- tools/urls.py
+++ tools/urls.py
@@ -16,16 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('sso/', include('esi.urls')),
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("sso/", include("esi.urls")),
     path("authentication/", include("eve_auth.urls")),
-    path('contracts/', include('contracts_v2.urls')),
-    path('referrals/', include('referrals.urls')),
-    path('fleets/', include('fleets.urls')),
-    path('rosters/', include('rosters.urls')),
-    path('logistics/', include('logistics.urls')),
-    path('intel/', include('intel.urls')),
-]
+    path("contracts/", include("contracts_v2.urls")),
+    path("referrals/", include("referrals.urls")),
+    path("fleets/", include("fleets.urls")),
+    path("rosters/", include("rosters.urls")),
+    path("logistics/", include("logistics.urls")),
+    path("intel/", include("intel.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
